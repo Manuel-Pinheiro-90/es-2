d0a1354777956408eafed1b15b83486ab9bb21e8
Manuel Pinheiro <manuel.pinheiro@libero.it>
2024-07-02 15:01:09 +0200

    Aggiungere i file di progetto.

---
+++ SQLQuery2.sql
@@ -0,0 +1,51 @@
+﻿--1
+SELECT
+ProductID, ProductName, SupplierID, CategoryID, QuantityPerUnit, UnitPrice, UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued
+
+FROM Products
+
+--2
+SELECT
+ProductID, ProductName, SupplierID, CategoryID, QuantityPerUnit, UnitPrice, UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued
+FROM Products
+WHERE UnitsInStock>40
+
+--3
+SELECT
+ EmployeeID, LastName, FirstName, Title, TitleOfCourtesy, BirthDate, HireDate, Address, City, Region, PostalCode, Country, HomePhone, Extension, Photo, Notes, ReportsTo, PhotoPath
+
+FROM Employees
+
+WHERE City = 'London'
+
+--4
+SELECT
+OrderID, CustomerID, EmployeeID,  OrderDate, RequiredDate, ShippedDate, ShipVia, Freight,  ShipName, ShipAddress, ShipCity, ShipRegion, ShipPostalCode, ShipCountry
+FROM Orders
+ORDER BY Freight DESC
+
+--5
+SELECT
+OrderID, CustomerID, EmployeeID,  OrderDate, RequiredDate, ShippedDate, ShipVia, Freight,  ShipName, ShipAddress, ShipCity, ShipRegion, ShipPostalCode, ShipCountry
+FROM Orders
+--WHERE Freight > 90 AND Freight < 200
+WHERE Freight BETWEEN 90 AND 200
+
+--6
+SELECT
+ProductID, ProductName, SupplierID, CategoryID, QuantityPerUnit, UnitPrice, UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued
+FROM Products
+WHERE CategoryID = 1
+
+--7
+SELECT
+OrderID, ProductID, UnitPrice, Quantity, Discount
+FROM [Order Details]
+WHERE Discount<>0
+
+SELECT
+OrderID, CustomerID, EmployeeID,  OrderDate, RequiredDate, ShippedDate, ShipVia, Freight,  ShipName, ShipAddress, ShipCity, ShipRegion, ShipPostalCode, ShipCountry
+FROM Orders
+Where CustomerID= 'BOTTM'
+AND Freight>50
+
